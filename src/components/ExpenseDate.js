c2d77c804c8f10e43af67183d7f0e32de3fde0c1
ogeeDeveloper <omarogrant202@hotmail.com>
2021-08-13 18:47:31 -0500

    Added styles to ExpenseDate component

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,16 +1,17 @@
+import "./ExpenseDate.css"
 function ExpenseDate({date}) {
     const month = date.toLocaleString('en-US', {month:'long'})
     const day = date.toLocaleString('en-US', {day:'2-digit'})
     const year = date.getFullYear()
     return (
-        <div>
-            <div>
+        <div className="expense-date">
+            <div className="expense-date__month">
                 {month}
             </div>
-            <div>
+            <div className="expense-date__year">{year}</div>
+            <div className="expense-date__day">
                 {day}
             </div>
-            <div>{year}</div>
         </div>
     )
 }
